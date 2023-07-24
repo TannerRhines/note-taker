a37f2f10d625d07ac5d95f0692fb809fb9e1fa54
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-24 01:09:06 -0800

    created function to save notes to db.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const allNotes = require('/db/db.json');
+const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +17,30 @@ app.get('/api/notes', (req, res) => {
 
 
 
+// function to add new note to db.json 
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+    notesArray = [];
+    
+    if (notesArray.length === 0)
+    notesArray.push(0);
+    
+    body.id = notesArray[0];
+    notesArray[0]++;
+    
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+        );
+        return newNote;
+}
+    
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
 
 
 
@@ -24,19 +48,11 @@ app.get('/api/notes', (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -49,12 +65,6 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
