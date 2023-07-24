26375db0165ec02befad10fcf7c0bd9df4c5d819
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-24 01:17:08 -0800

    added deleteNote function

--- server.js
+++ server.js
@@ -43,7 +43,21 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+        
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+                );
+                
+                break;
+            }
+        }
+}
 
 
 
