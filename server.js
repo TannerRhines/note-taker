555afb8539817d06fb5dfcd68fa2713daf0c57d4
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-24 00:42:02 -0800

    added front end routes (index, notes.html)

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
+const allNotes = require('/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
 
 
 
